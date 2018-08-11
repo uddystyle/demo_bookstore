13953dbfe964f8ccc869bbac56948bd20927adb3
Tomohisa Uchida <uchida@uddystyle.com>
2018-08-12 03:36:02 +0900

    Added search controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'books#index'
 
   resources :books
+  resources :searches
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
